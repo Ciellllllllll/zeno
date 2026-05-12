9ca9aa3dd59a8968ffcb23025d1697974d2bdbfb
KeisukeKono_SUB <cielgameee@gmail.com>
2026-05-13 08:02:10 +0900

    Add minimal engine loop

--- engine_core/src/main.rs
+++ engine_core/src/main.rs
@@ -1,5 +1,13 @@
 fn main(){
     println!("Zeno Engine Core started");
 
+    println!("Engine loop started");
+
+    for n in 1..61{
+        println!("Frame {} update", n);
+    }
+
+    println!("Engine loop finished");
+
     println!("Zeno Engine Core shutdown");
 }
